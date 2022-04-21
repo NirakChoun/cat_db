cdac9b7c93fc86648ef3c4e2b44758b70923f6a1
NirakChoun <100827012+rakzer0@users.noreply.github.com>
2022-04-21 21:59:15 +0700

    Add files via upload

---
+++ db.py
@@ -0,0 +1,74 @@
+import mysql.connector
+
+'''
+State your group member name and id here:
+Song Rithykun 
+Socheata Thy
+Choun Chan Nirak 2022201
+'''
+
+
+# TODO:
+# host can be 'localhost' or '127.0.0.1'
+# if you are using mamp, password is root
+# and port is 8889
+# use cat_db as database.
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="cat_db",
+    port="8889"
+)
+
+
+cursor = mydb.cursor()
+
+
+def register_cat(cat_info):
+    sql = "INSERT INTO cats (name, gender, breed, dob, description) VALUES (%s, %s, %s, %s, %s)"
+    cursor.execute(sql, cat_info)
+
+    mydb.commit()
+    print("Register Completed!\n")
+
+
+
+def get_cats():
+    sql = "SELECT * FROM cats"
+    cursor.execute(sql)
+    myresult = cursor.fetchall()
+
+    return(myresult)
+
+
+
+def get_cat(id):
+    sql = "SELECT * FROM cats"
+    cursor.execute(sql)
+    myresult = cursor.fetchone()
+
+    return(myresult)
+
+
+def update_cat(cat_info):
+
+    id, name, gender, breed, dob, description = cat_info
+    
+    sql = f"UPDATE cats SET name = '{name}', gender = '{gender}', breed = '{breed}', dob = '{dob}', description = '{description}' WHERE id = '{id}'"
+
+    cursor.execute(sql)
+
+    mydb.commit()
+
+
+def remove_cat(id):
+    
+    sql = "DELETE FROM cats WHERE id = {id}"
+
+    cursor.execute(sql)
+
+    mydb.commit()
+
+
+
